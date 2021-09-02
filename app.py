72ae84178d57aa7fc38150a9f653485427f38291
Mint014 <89176620+Mint014@users.noreply.github.com>
2021-09-02 10:48:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Hello เปรมมิกา พันธุ์พิริยะ เลขที่14 ชั้นม.4/6"
 
 @app.route('/api', methods=['GET'])
 def get_api():
